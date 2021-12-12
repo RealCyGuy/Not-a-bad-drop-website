9cbc0199f5e75370042536bcfc31e8fae873744a
Cyrus Yip <cyruscmyip1@gmail.com>
2021-12-12 10:04:44 -0800

    unfinshed

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Work Sans", "sans-serif"],
+      serif: ["Roboto Slab", "Georgia", "serif"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
